9366a0868477b023c478b737dda72bcc49ac38a0
samuele794 <samuelebruschi794@gmail.com>
2025-10-29 16:11:08 +0100

    Add kotlinx immutable

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,6 +56,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.immutable)
             implementation(project(":os-map"))
         }
         commonTest.dependencies {
