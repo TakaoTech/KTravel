078695232e0ebb14a11b2b8ff56cc8b7600bad7e
samuele794 <samuelebruschi794@gmail.com>
2026-02-01 16:04:55 +0100

    Removed workflow consistency check

--- .github/workflows/detekt.main.kts
+++ .github/workflows/detekt.main.kts
@@ -19,13 +19,15 @@ import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.ConsistencyCheckJobConfig
 
 workflow(
     name = "Detekt",
     on = listOf(
         Push(branches = listOf("main", "dev")),
     ),
-    sourceFile = __FILE__
+    sourceFile = __FILE__,
+    consistencyCheckJobConfig = ConsistencyCheckJobConfig.Disabled
 ) {
     job(id = "detekt", runsOn = RunnerType.UbuntuLatest) {
         uses(name = "Checkout code", action = Checkout())
