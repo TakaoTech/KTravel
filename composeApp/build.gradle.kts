6d35c6cf2b2fa3144c0b0ff3bcf4f07171228b9d
samuele794 <samuelebruschi794@gmail.com>
2026-03-04 16:30:02 +0100

    Change compile target to Java 21

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
@@ -173,8 +173,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
