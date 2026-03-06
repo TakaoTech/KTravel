fe8f1c45753f83e1298b09b0c795d0eb0634930a
samuele794 <samuelebruschi794@gmail.com>
2026-03-06 09:51:48 +0100

    Revert "Refactor navigation graphs by modularizing and delegating to individual `NavGraphBuilder` extensions."
    This reverts commit 3485465295b501c2e581059724a7d63c1e9ac41a.

--- composeApp/src/commonMain/kotlin/com/takaotech/ktravel/navigation/AppNavGraph.kt
+++
@@ -1,9 +0,0 @@
-package com.takaotech.ktravel.navigation
-
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
-import com.takaotech.ktravel.ui.planning.navigation.planningNavGraph
-
-fun NavGraphBuilder.appNavGraph(navController: NavHostController) {
-    planningNavGraph(navController)
-}
