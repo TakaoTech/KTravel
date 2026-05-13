a3f42e33de2c94acd5309262d9ff85edffe5d357
samuele794 <samuelebruschi794@gmail.com>
2026-05-13 15:48:45 +0200

    Complete migration to AGP9

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
@@ -12,7 +11,6 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.takaotech.ktravel"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = libs.versions.ktravel.code.get().toInt()
