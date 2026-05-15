2d38f3d1ba5353c16601927b7ed09ba4f1340e80
samuele794 <samuelebruschi794@gmail.com>
2026-05-15 15:15:53 +0200

    WIP Kin Migration to 4.2.0 and compiler

--- androidApp/src/main/kotlin/com/takaotech/ktravel/KTravelApplication.kt
+++ androidApp/src/main/kotlin/com/takaotech/ktravel/KTravelApplication.kt
@@ -1,7 +1,16 @@
 package com.takaotech.ktravel
 
 import android.app.Application
+import com.takaotech.ktravel.di.startKTravelKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 
 class KTravelApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        startKTravelKoin {
+            androidContext(this@KTravelApplication)
+            androidLogger()
+        }
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.composeHotReload) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
@@ -15,6 +14,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotest) apply false
     alias(libs.plugins.kotzilla) apply false
+    alias(libs.plugins.kotzilla.koin.compiler) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.stability.analyzer) apply false
     alias(libs.plugins.mokkery) apply false
