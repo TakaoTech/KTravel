36417d0f3e872189ce1993e961c6a4b9d2eb8351
samuele794 <samuelebruschi794@gmail.com>
2026-06-15 10:27:22 +0200

    Remove CompositionLocalProvider and AppGraph initialization
    - Eliminate `LocalAppGraph` and `CompositionLocalProvider` from app setup.
    - Simplify `AppGraph` initialization by using `remember` and direct `createAppGraph` calls.
    - Refactor `main`, `MainActivity`, `MainViewController`, and `App` for improved clarity and reduced boilerplate.

--- androidApp/src/main/kotlin/com/takaotech/ktravel/MainActivity.kt
+++ androidApp/src/main/kotlin/com/takaotech/ktravel/MainActivity.kt
@@ -4,26 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.CompositionLocalProvider
-import com.takaotech.ktravel.di.LocalAppGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
-        val appGraph = (application as KTravelApplication).appGraph
-
-        setContent {
-            CompositionLocalProvider(LocalAppGraph provides appGraph) {
-                App()
-            }
-        }
+        setContent { App() }
     }
 }
-
-//@Preview
-//@Composable
-//fun AppAndroidPreview() {
-//    App()
-//}
